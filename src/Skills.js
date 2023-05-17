a3a86b135bbfad85a1bd040d76f1e0ba39b0a705
RavendraDotJava <rp7098979@gmail.com>
2023-05-17 12:19:29 -0700

    sixth Change

--- src/Skills.js
+++ src/Skills.js
@@ -4,7 +4,7 @@ function Skills(props) {
   return (
     <>
       <div className="skillcard">
-        <img className="skillimg" src={props.url} />
+        <img className="skillimg" src={props.url} alt="Skill" />
         <p style={{ fontSize: "20px" }}>{props.skname}</p>
       </div>
     </>
